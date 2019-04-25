bc51b05446ceb75fb766839a25c3273d71bd3f72
Nolan <hedglin.nolan>
2019-04-25 13:15:17 -0400

    initiated GSW file

---
+++ GSW.py
@@ -0,0 +1,9 @@
+'''This file is an implementation of Gentry-Sahai-Waters (GSW) 2011 fully homomorphic encryption scheme.
+GSW transforms a plaintext bit into a ciphertext matrix and relies on the Decisional Learning With Errors
+assumption for computational security.
+
+Authors: Nolan Hedglin, Kade Phillips, Andrew Reilley
+Institution: MIT'''
+
+import numpy as np
+
