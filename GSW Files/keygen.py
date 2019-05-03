961a14a06f5cb8e0ccad69259b88a38f65591998
nhedglin <35072760+hedglinnolan@users.noreply.github.com>
2019-05-03 15:14:25 -0400

    Added functions to utils, keygen

--- GSW Files/keygen.py
+++ GSW Files/keygen.py
@@ -1,43 +1,20 @@
 '''This file implements key generation for GSW encryption.'''
 
 import numpy as np
-import random
+import random    
+ 
 
-# Python 3 program to print all sophie  
-# german prime number till n. 
-  
-# Function to detect prime number 
-def sieve(n, prime) : 
-    p = 2
-    while( p * p <= n ): 
-        # If prime[p] is not changed,   
-        # then it is a prime 
-        if (prime[p] == True) : 
-              
-            # Update all multiples of p 
-            for i in range(p * 2, n, p) : 
-                prime[i] = False
-                  
-        p += 1
-          
-                  
-def printSophieGermanNumber(n) : 
-    # We have made array till 2*n +1 
-    # so that we can check prime number 
-    # till that and conclude about sophie 
-    # german prime . 
-    prime = [True]*(2 * n + 1) 
-      
-    sieve(2 * n + 1, prime) 
-  
-    for i in range(2, n + 1) : 
-          
-        # checking every i whether it is 
-        # sophie german prime or not. 
-        if (prime[i] and prime[2 * i + 1]) : 
-            print(i) 
-              
-  
-# Driver Code 
-n = 25
-printSophieGermanNumber(n) 
+# Keygen function
+def keyGen(k):
+	''' Generates the secret key and pulic key given a security parameter k.
+
+	Input: k - security parameter (i.e. # of bits in key)
+	Output: sk - secret key
+			pp = (n.q) - parameters known to the public'''
+	q_array = generateSophieGermainPrimes(k)
+	q = random.choice(q_array)
+	l = np.ceil(q)
+	return q, l
+
+
+k = 20 # security parameter
--- GSW Files/util.py
+++ GSW Files/util.py
@@ -1,6 +1,37 @@
 import numpy as np
 from scipy.linalg import block_diag
 
+# Function to detect prime number 
+def sieve(n, prime): 
+    p = 2
+    while( p * p <= n ): 
+        # If prime[p] is not changed,   
+        # then it is a prime 
+        if (prime[p] == True): 
+              
+            # Update all multiples of p 
+            for i in range(p * 2, n, p): 
+                prime[i] = False
+        p += 1
+          
+                  
+def generateSophieGermainPrimes(k): 
+	''' We have made array till 2*n +1 so that we can check prime number till that and conclude about sophie german prime .
+		
+	Input: k - security parameter
+	Output: q_array - randomly selects q from array of sophie germain primes'''
+
+	q_list = []
+	n = 2 ** k # representing k-bit number in base 10
+	prime = [True]*(2 * n + 1) 
+	sieve(2 * n + 1, prime) 
+	for i in range(2, n + 1): # checking every i whether it is sophie german prime or not. 
+		if (prime[i] and prime[2 * i + 1]): 
+			q_list.append(i)
+	q_array = np.array(q_list)
+	return q_array
+
+
 def buildGadget(l, n): # Returns the gadget matrix for a given modulus
 	g = np.linspace(0, l - 1, l)[np.newaxis]
 	print(g.shape)
