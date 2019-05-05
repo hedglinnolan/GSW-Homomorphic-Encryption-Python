3b813bf989f08d2e86cfdeb605dad180480046c1
Nolan <hedglin.nolan@gmail.com>
2019-05-05 18:47:15 -0400

    Made gaussian discrete for error vec

--- GSW Files/keygen.py
+++ GSW Files/keygen.py
@@ -19,9 +19,9 @@ def keyGen(k):
 		t[i] = int(float(FFE(random.randrange(q),q))*(-1))
 	t[k-1] = 1
 	S = t[:k-1]*(-1)
-	e = np.random.normal(size = int(m))
+	e = np.rint(np.random.normal(size = int(m)))
 	A = np.array([[float(FFE(random.randrange(q),q)) for y in range(int(k-1))] for x in range(int(m))])
-	SAE = np.dot(S,np.transpose(A)) + e# Calculating S*A+e
+	SAE = np.dot(S,np.transpose(A)) + e # Calculating S*A+e
 	SAE = np.transpose([SAE]) # This dumb line of code is so the dimensions work out. I hate python so much (#matlab4lyfe)
 	B = np.transpose(np.hstack([A,SAE]))
 	return t,B,e
@@ -30,7 +30,7 @@ def keyGen(k):
 k = [7] # security parameter
 
 for i in k:
-	t,B, e = keyGen(i)
+	t, B, e = keyGen(i)
 	#Check for correctness (t*B = e ideally)
 	check = np.dot(t,B)
 	print(check)
