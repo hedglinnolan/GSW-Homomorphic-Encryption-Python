30dc7ecf2bb1b8b59a49dfb7f35c62f2d2a539cc
Nolan <hedglin.nolan@gmail.com>
2019-05-05 19:07:07 -0400

    all dtype is int

--- GSW Files/keygen.py
+++ GSW Files/keygen.py
@@ -11,23 +11,24 @@ def keyGen(k):
 		Output: t,B - secret and public key
 	'''
 	q_array = generateSafePrimes(k)
-	q = random.choice(q_array)
-	l = np.ceil(np.log2(q))
+	q = random.choice(q_array).astype(np.int64)
+	l = np.ceil(np.log2(q)).astype(np.int64)
 	m = k*l
-	t = np.arange(k)
+	print(m.dtype)
+	t = np.arange(k, dtype = np.int64)
 	for i in range(k-1):
-		t[i] = int(float(FFE(random.randrange(q),q))*(-1))
+		t[i] = int(FFE(random.randrange(q),q))*(-1)
 	t[k-1] = 1
 	S = t[:k-1]*(-1)
-	e = np.rint(np.random.normal(size = int(m)))
-	A = np.array([[float(FFE(random.randrange(q),q)) for y in range(int(k-1))] for x in range(int(m))])
+	e = np.rint(np.random.normal(size = int(m))).astype(np.int64)
+	A = np.array([[int(FFE(random.randrange(q),q)) for y in range(int(k-1))] for x in range(int(m))])
 	SAE = np.dot(S,np.transpose(A)) + e # Calculating S*A+e
 	SAE = np.transpose([SAE]) # This dumb line of code is so the dimensions work out. I hate python so much (#matlab4lyfe)
 	B = np.transpose(np.hstack([A,SAE]))
 	return q,t,B,e
 
 
-k = [7] # security parameter
+k = [25] # security parameter
 
 for i in k:
 	q, t, B, e = keyGen(i)
