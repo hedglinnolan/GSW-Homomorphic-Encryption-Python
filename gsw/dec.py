821446aa4babd1f956b8eaa59029b34e2270d6f8
kade <kade@mit.edu>
2019-05-14 23:35:56 -0400

    yay

--- gsw/dec.py
+++ gsw/dec.py
@@ -11,7 +11,7 @@ def decrypt(keys, ciphertext):
     msg  = np.dot(keys.SK, ciphertext) % keys.q
     g = buildGadget(keys.l, keys.n)
     sg = np.dot(keys.SK, g) % keys.q
-    div = np.rint(msg / sg).astype(np.int64)
+    div = np.rint((msg / sg).astype(np.float)).astype(np.int64)
     modes = np.unique(div, return_counts=True)
     modes = sorted(zip(modes[0], modes[1]), key = lambda t: -t[1])
     best_num = 0
@@ -19,7 +19,8 @@ def decrypt(keys, ciphertext):
     for mu,count in modes:
         dist = (msg - mu*sg) % keys.q
         dist = np.minimum(dist, keys.q - dist)
-        dist = np.linalg.norm(dist)
+        #dist = np.linalg.norm(dist)
+        dist = np.dot(dist, dist)
         if dist < best_dist:
             best_num = mu
             best_dist = dist
